df579207c5ee8a2d3697ede005ffa3a19e527eb8
rohit1998 <gupta.rohit21198@gmail.com>
2018-01-28 18:37:58 +0530

    done

--- tokenizer.py
+++ tokenizer.py
@@ -1,16 +1,6 @@
-###########################################
-# Oberon Compiler Project
-# CS 335: Principles Of Compiler Design
-# Group - 5
-# Ankit Kumar, Rahul Varma, Raghubansh Mani
-# Suralkar Vijay Suresh, Vikas Wasiya
-###########################################
-import lex
 
-# reserved keywords
 reserved = {
 	'ABS' : 'ABS',
-	'AND' : 'AND',
 	'ARRAY' : 'ARRAY',
 	'BEGIN' : 'BEGIN',
 	'BOOLEAN' : 'BOOLEAN',
@@ -43,7 +33,6 @@ reserved = {
 	'NOT' : 'NOT',
 	'ODD' : 'ODD',
 	'OF' : 'OF',
-	'OR' : 'OR',
 	'ORD' : 'ORD',	
 	'POINTER' : 'POINTER',
 	'PROCEDURE' : 'PROCEDURE',
@@ -61,86 +50,69 @@ reserved = {
 	'WHILE' : 'WHILE',
 	'WRITE' : 'WRITE',
 	'WRITELN' : 'WRITELN'
-}
+   }
 
-# Operators
-t_PLUS = r'\+'
-t_MINUS = r'-'
-t_MULTIPLY = r'\*'
-t_DIVIDE = r'/'
-t_MODULUS = r'%'
-t_DOT = r'\.'
+class tokenizer(object):
+        # Operators
+        t_ignore = ' \t'
+        t_PLUS = r'\+'
+        t_MINUS = r'-'
+        t_MULTIPLY = r'\*'
+        t_DIVIDE = r'/'
+        t_MODULUS = r'%'
+        t_DOT = r'\.'
+        
+        # Relation
+        t_ASSIGN = r':='
+        t_EQUAL = r'='
+        t_NEQUAL = r'!='
+        t_LT = r'<'
+        t_GT = r'>'
+        t_LTEQ = r'<='
+        t_GTEQ = r'>='
 
-# Relation
-t_ASSIGN = r':='
-t_EQUAL = r'='
-t_NEQUAL = r'!='
-t_LT = r'<'
-t_GT = r'>'
-t_LTEQ = r'<='
-t_GTEQ = r'>='
+        # BOOLEAN
+        t_OR = r'\|'
+        t_AND = r'&'
 
-
-
-#t_ignore = ' \t'
-# BOOLEAN
-t_OR = r'\|'
-t_AND = r'&'
-t_IN = r'IN'
-t_IS = r'IS'
-
-# Delimiters
-t_COMMA = r','
-t_SCOLON = r';'
-t_LSB = r'\['
-t_RSB = r'\]'
-t_LRB = r'\('
-t_RRB = r'\)'
-t_LCB = r'{'
-t_RCB = r'}'
+        # Delimiters
+        t_COMMA = r','
+        t_SCOLON = r';'
+        t_COLON = r':'
+        t_LSB = r'\['
+        t_RSB = r'\]'
+        t_LRB = r'\('
+        t_RRB = r'\)'
+        t_LCB = r'{'
+        t_RCB = r'}'
 
 
-t_VAL_INTEGER = r'[0-9]+'
-t_VAL_REAL = r'[0-9]+\.[0-9]+'
-t_VAL_BOOLEAN = r'TRUE|FALSE'
-#t_DOT_DOT = r'\.\.'
-t_VAL_STRING = r'\".*?\"'
+        t_VINTEGER = r'[0-9]+'
+        t_VREAL = r'[0-9]+\.[0-9]+'
+        t_VBOOLEAN = r'TRUE|FALSE'
+        t_VSTRING = r'\".*?\"'
+        t_VCHAR = r'(L)?\'(.|\n)\''
+        tokens = ['ASSIGN', 'LT' , 'GT' , 'PLUS' , 'MINUS' , 'MULTIPLY' , 'DIVIDE' , 'MODULUS' , 'OR' , 'AND' , 'EQUAL' ,  'NEQUAL' , 'DOT' , 'COMMA' , 'SCOLON' , 'LSB' , 'RSB' , 'LRB' , 'RRB' , 'LCB' , 'RCB' , 'LTEQ', 'GTEQ', 'IDENT', 'VINTEGER', 'VREAL', 'VSTRING', 'VBOOLEAN', 'VCHAR', 'DOT_DOT' , 'COLON' ] + list(reserved.values())
+        def t_IDENT(self, t):
+                r'[a-zA-Z]([a-zA-Z0-9])*'
+                t.type = reserved.get(t.value,'IDENT')
+                return t
 
-#literals = "+-*/~&.,;|([{}]):><#=^"
-#t_VAL_CHAR = r'(L)?\'(.|\n)\''
-   
-tokens = ['ASSIGN', 'LT' , 'GT' , 'PLUS' , 'MINUS' , 'MUULTIPLY' , 'DIVIDE' , 'MODULUS' , 'OR' , 'AND' , 'EQUAL' , 'IN' , 'IS' , 'NEQUAL' , 'DOT' , 'COMMA' , 'SCOLON' , 'LSB' , 'RSB' , 'LRB' , 'RRB' , 'LCB' , 'RCB' , 'LTEQ', 'GTEQ', 'IDENT', 'VAL_INTEGER', 'VAL_REAL', 'VAL_STRING', 'VAL_BOOLEAN', 'VAL_CHAR', 'DOT_DOT' ] + list(reserved.values())
+        def t_comment(self, t):
+                r'(\(\*(.|\n)*?\*\))'
+                t.lexer.lineno += t.value.count('\n')
+                pass
+        
+        def t_newline(self, t):
+                r'\n+'
+                t.lexer.lineno += len(t.value)
 
-def t_IDENT(t):
-	r'[a-zA-Z]([a-zA-Z0-9])*'
-	t.type = reserved.get(t.value,'IDENT')
-	#if t.value == 'TRUE' or t.value == 'FALSE':
-		#t.type = 'VAL_BOOLEAN'
-	return t
-
-#t_ignore_comment = r'(\(\*(.|\n)*?\*\))'
-def t_comment(t):
-	r'(\(\*(.|\n)*?\*\))'
-	t.lexer.lineno += t.value.count('\n')
-	pass #ignore a comment line
-
-def t_newline(t):
-	r'\n+'
-	t.lexer.lineno += len(t.value)
-
-# error handling : skip unwanted charater after printing a message
-def t_error(t):
-	last_cr = t.lexer.lexdata.rfind('\n',0,t.lexpos)
-	if (last_cr < 0) :
-		last_cr = -1		
-	else :
-		last_cr = last_cr 
-	columnNo = (t.lexpos - last_cr)
-	print "Illegal character",t.value[0],"at line : ",t.lexer.lineno,"column",columnNo,": Skipping"
-	t.lexer.skip(1)
-
-lexer = lex.lex()
-
-# if called from main check the working of this lexer
-if __name__ == '__main__' :
-	lex.runmain()
+        def t_error(self, t):
+                last_cr = t.lexer.lexdata.rfind('\n',0,t.lexpos)
+                if (last_cr < 0) :
+                        last_cr = -1		
+                else :
+                        last_cr = last_cr 
+                columnNo = (t.lexpos - last_cr)
+                print ("Illegal character",t.value[0],"at line : ",t.lexer.lineno,"column",columnNo,": Skipping")
+                t.lexer.skip(1)
---
+++ untitled.py
@@ -0,0 +1,38 @@
+import os.path
+import sys
+import lex
+from tokenizer import tokenizer
+
+def Print():
+    print('%20s   %20s   %20s'%("Token", "Occurrances", "Lexemes",))
+    print('_'*70)
+    print()
+
+    for i,j in table.items():
+        if j[0] == 0:
+            continue
+        else:
+            k = list(j[1])
+            l = len(k)
+            print('%20s    %20d   %20s'%(i, j[0], k[0],))
+            for x in range(1,l):
+                print('%20s    %20s   %20s'%("", "", k[x],))
+
+if __name__ == "__main__":
+    tokens = tokenizer.tokens
+    table = { k:[0,set()] for k in tokens}   
+    lexer = lex.lex(module=tokenizer())
+    filename = sys.argv[1]
+    if os.path.exists(filename):
+        file = open(filename, 'r')
+        data = file.read()
+        lexer.input(data)
+        while True:
+            tokk = lexer.token()
+            if not tokk: break
+            table[tokk.type][0] += 1
+            table[tokk.type][1].add(tokk.value)
+        file.close()
+    else:
+        print("File Does Not Exists ...")    
+    Print()
