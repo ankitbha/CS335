0e1504855e02cbe1fa1c1e0e2af5da710e86b114
rohit1998 <gupta.rohit21198@gmail.com>
2018-02-05 22:58:37 +0530

    leaders done

--- src/codegen.py
+++ src/codegen.py
@@ -1,16 +1,16 @@
 import sys
 
 def inputIR():
-	pass
+    pass
 
 def getReg():
-	pass
+    pass
 
 def setLoc():
-	pass
+    pass
 
 def getLoc():
-	pass
+    pass
 
 def isInt(s):
     try: 
@@ -20,54 +20,77 @@ def isInt(s):
         return False
 
 def main():
-	if len(sys.argv) == 2:
-	    filename = str(sys.argv[1])
-	else:
-	    print("Too many or too few arguements")
-	    exit()
+    if len(sys.argv) == 2:
+        filename = str(sys.argv[1])
+    else:
+        print("Too many or too few arguements")
+        exit()
 
-	incode = open(filename).read().splitlines()
-	print(incode)
+    incode = open(filename).read().splitlines()
+    print(incode)
 
-	for line in incode:
-		line = line.split(', ')
-		for var in line:
-			if(var not in reserved and var not isInt(var)):
-				#add var to variable list
-				#add in addressdisp ans symbl table
+    leaders = [1,]
+    variables = []
+    
+    for line in incode:
+        l = line.split(', ')
+        for var in l:
+            if(var not in reserved and not isInt(var)):
+                variables.append(var)
+                #add var to variable list
+                #add in addressdisp ans symbl table
+    variables = list(set(variables))
+    print(variables)
+    for line in incode:
+        l = line.split(', ')
+        if 'ifgoto' in l:
+            leaders.append(int(l[-1]))
+            leaders.append(int(l[0])+1)
+        
+        elif 'goto' in l:
+            leaders.append(int(l[-1]))
+            leaders.append(int(l[0])+1)
+        
+        elif 'function' in l:
+            leaders.append(int(l[0]))
+        
+        elif 'label' in l:
+            leaders.append(int(l[0]))
+    leaders = list(set(leaders))
+    leaders.sort()
+    print(leaders)
 
-
-# 	for instr in instrlist:
-# 	templist = instr.split(', ')
-# 	if templist[1] not in ['label', 'call', 'function']:
-# 		varlist = varlist + templist 
+#   for instr in instrlist:
+#   templist = instr.split(', ')
+#   if templist[1] not in ['label', 'call', 'function']:
+#       varlist = varlist + templist 
 # varlist = list(set(varlist))
 # varlist = [x for x in varlist if not isnumber(x)]
 # for word in tackeywords:
-# 	if word in varlist:
-# 		varlist.remove(word)
+#   if word in varlist:
+#       varlist.remove(word)
 # addressDescriptor = addressDescriptor.fromkeys(varlist, "mem")
 # symbolTable = addressDescriptor.fromkeys(varlist, ["live", None])
 
 
 
 if __name__ == "__main__":
-	keyword = ['ifgoto', 'goto', 'return', 'call', 'print', 'label', 'function', 'exit']
-	relation = ['<=', '>=', '==', '>', '<', '!=', '=']
-	mathop = ['+', '-', '*', '/', '%']
+    keyword = ['ifgoto', 'goto', 'return', 'call', 'print', 'label', 'function', 'exit', 'return']
+    relation = ['<=', '>=', '==', '>', '<', '!=', '=']
+    mathop = ['+', '-', '*', '/', '%']
     boolop = ['&', '|', '!']
     reserved = keyword + relation + mathop + boolop
-    
+
     main()
 
 
-	
+    
 
 # if len(sys.argv) == 2:
-# 	filename = str(sys.argv[1])
+#   filename = str(sys.argv[1])
 # else:
-# 	print("usage: python codegen.py irfile")
-# 	exit()
+#   print("usage: python codegen.py irfile")
+#   exit()
 
 # irfile = open(filename, 'r')
 # ircode = irfile.read()
@@ -81,13 +104,13 @@ if __name__ == "__main__":
 
 # # Construct the variable list and the address discriptor table
 # for instr in instrlist:
-# 	templist = instr.split(', ')
-# 	if templist[1] not in ['label', 'call', 'function']:
-# 		varlist = varlist + templist 
+#   templist = instr.split(', ')
+#   if templist[1] not in ['label', 'call', 'function']:
+#       varlist = varlist + templist 
 # varlist = list(set(varlist))
 # varlist = [x for x in varlist if not isnumber(x)]
 # for word in tackeywords:
-# 	if word in varlist:
-# 		varlist.remove(word)
+#   if word in varlist:
+#       varlist.remove(word)
 # addressDescriptor = addressDescriptor.fromkeys(varlist, "mem")
 # symbolTable = addressDescriptor.fromkeys(varlist, ["live", None])
